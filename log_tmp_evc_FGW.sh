e6d12e74a5050cb527e98e19a6f335447f73b760
harakzf <14cc123f@al.rikkyo.ac.jp>
2020-04-25 12:37:10 +0900

    all sh change

--- log_tmp_evc_FGW.sh
+++ log_tmp_evc_FGW.sh
@@ -11,7 +11,8 @@ if [ ${logs} = "/usr/ap/bambi/log/*.log" ]; then
     echo "退避するログファイルはありませんでした。"
     break
 else
-    select data in fais2 docomo eastwest # データ選択
+    # all_Performance_d_ew ：性能試験用関戸さんデータ（ドコモ/東西）
+    select data in fais2 fais5 docomo eastwest all_Performance_d_ew # データ選択
     do #doからdoneまでループ処理
         declare directory=logbk_`date "+%Y%m%d"`_${data}_`date "+%H%M"`
 
